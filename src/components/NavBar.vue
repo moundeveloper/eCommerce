3c107a0ea781ec374ea9d6fb9794f29d7081b84c
MrRobot996 <guiseddu@gmail.com>
2023-04-17 10:30:58 +0200

    update home 1
    primo update home

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,18 @@
 <template>
-    <!-- Contenuto: logo, nav con i link & link fuori dalla nav per il carrello -->
-    <nav class="flex gap-4 justify-center">
-        <router-link to="/">home</router-link>
-        <router-link to="/customize-shirt">customize</router-link>
-        <router-link to="/catalog">catalog</router-link>
-        <router-link to="/cart">cart</router-link>
+  <!-- Contenuto: logo, nav con i link & link fuori dalla nav per il carrello -->
+  <nav class="flex gap-4 justify-center">
+    <router-link to="/">Home</router-link>
+    <router-link to="/customize-shirt">Customize</router-link>
+    <router-link to="/catalog">Catalog</router-link>
+    <router-link to="/cart">Cart</router-link>
 
-        <!-- Le icone si usano in questo modo e le cercate da https://oh-vue-icons.js.org/docs 
+    <!-- Le icone si usano in questo modo e le cercate da https://oh-vue-icons.js.org/docs 
             il name delle icone è sempre in lower-case con il trattino
         -->
-        <v-icon name="md-add" />
-    </nav>
+    <v-icon name="md-add" />
+  </nav>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style scoped></style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,59 @@
 <template>
-    <div class="h-full">
-        Home
+  <div class="h-full flex justify-center">
+    <div class="custom-container">
+      <div class="left-part">
+        <h1>
+          We are looking <br />
+          to make you <br />
+          Awesome
+        </h1>
+        <p>
+          Eleva il tuo stile con la nostra collezione di magliette <br />
+          personalizzate, create su misura per esprimere la tua unicità e il tuo
+          <br />
+          spirito creativo.
+        </p>
+      </div>
+      <div class="right-part">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHMKLi3j-EDHh86ytdhRM1rQh8S41CJ7RhSGTu5nyi11wlNZLMzl0G4JrUL5k8rFocFzI&usqp=CAU"
+          alt=""
+        />
+        <div class="slider"></div>
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
+<style scoped>
+.custom-container {
+  display: flex;
+  justify-content: space-around;
 
-<style scoped></style>
+  width: 70rem;
+}
+.left-part {
+  gap: 30rem;
+  margin-top: 12rem;
+}
+h1 {
+  font-size: 45px;
+  font-family: Volkhov;
+  font-weight: 500;
+}
+p {
+  line-height: 1rem;
+  font-weight: 200;
+  font-size: 15px;
+  font-family: "Poppins", sans-serif;
+}
+.right-part {
+  margin-top: 12rem;
+  flex-direction: column;
+  align-items: center;
+}
+.slider {
+}
+</style>
